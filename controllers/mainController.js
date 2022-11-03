b593a2889e0e39bb3d47e5b25ffe98a8698770f5
paulbinot <paulbinot23@gmail.com>
2022-11-03 16:30:35 +0100

    ajout recherche sur index et page film

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,7 +2,15 @@ const films = require('../films.json');
 
 const mainController = {
   renderHomePage: (req, res) => {
-    res.render("index");
+    const searched = req.query.q;
+    if (!searched) {
+      res.render("index", {filteredFilms: films});
+      return;
+    }
+
+    const filteredFilms = films.filter(game => game.title.toLowerCase().includes(searched.toLowerCase()));
+
+    res.render("index", {filteredFilms});
   },
   renderFilmPage: (req, res) => {
     const filmData = films.find(film => film.title.replace(/\s/g, '-') === req.params.filmName);
---
+++ public/js/minutesToHHMM.js
@@ -0,0 +1,21 @@
+function minutesToHHMM(minutes) {
+  const numberMinutes = parseInt(minutes, 10);
+  const hours = Math.floor(numberMinutes / 60);
+  let restMinutes = numberMinutes%60;
+  if (restMinutes < 10) {
+    restMinutes = `0${restMinutes}`;
+  }
+  if (hours <= 0) {
+    return `${restMinutes} minutes`;
+  } else {
+    return `${hours}h${restMinutes}`;
+  }
+}
+
+const durations = document.querySelectorAll(".film__duration");
+
+
+durations.forEach(duration => {
+  const newDuration = minutesToHHMM(duration.textContent);
+  duration.textContent = newDuration;
+});
